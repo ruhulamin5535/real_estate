48c5aec6ee3250d8c7646c091b2d9cf50355f273
ruhulamin5535 <ruhulamincse15@gmail.com>
2019-10-08 23:49:08 +0600

    added readme

--- loginuser.php
+++ loginuser.php
@@ -146,7 +146,7 @@
 							<li><a href="index.html">HOME</a></li>
 							
 							<li><a href="about.html">ABOUT US</a></li>
-									<li><a href="contact.html">CONTACT</a></li>
+									<li><a href="contact.html">CONTACT us</a></li>
 						</ul>
 					</div>
 				</div>
